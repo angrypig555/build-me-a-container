b3c1dace90945172850432b9f2c2546f5404245c
angrypig555 <brny@brny.dev>
2026-02-14 20:37:16 +0000

    add extra func

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,26 @@
 #include <iostream>
 #include <fstream>
+#include <cstdlib>
 
 // all the strings for the input
 char yesno;
 std::string image_name;
 std::string work_dir;
 int work_dir_use;
+std::string copy_host_path;
+std::string copy_container_path;
+int copy_in_use;
 
 int main() {
     std::cout << "build-me-a-container 0.1, dockerfile generator" << std::endl;
+    std::cout << "WARNING! Before running this program, make sure you are in a seperate, empty directory." << std::endl;
     std::cout << "welcome user, this is an interactive prompt to generate and build a dockerfile" << std::endl;
     std::cout << "first off, lets start off with the very basics, what image will be the base of this container?" << std::endl;
     std::cout << "e.g alpine" << std::endl;
 image_name_input:
-    std::cout << "image name:";
+    std::cout << "image name: ";
     std::cin >> image_name;
-    std::cout << "are you sure [y/n]? image name:" << image_name << std::endl;
+    std::cout << "are you sure [y/n]? image name: " << image_name << std::endl;
     std::cin >> yesno;
     switch(yesno) {
         case 'y':
@@ -29,7 +34,7 @@ image_name_input:
     switch(yesno) {
         case 'y':
             work_dir_use = 1;
-            std::cout << "please specify working directory:" << std::endl;
+            std::cout << "please specify working directory: ";
             std::cin >> work_dir;
             std::cout << "set " << work_dir << " as working directory" << std::endl;
             break;
@@ -38,6 +43,7 @@ image_name_input:
             std::cout << "no working directory will be used" << std::endl;
             break;
     }
+
     std::cout << "creating dockerfile next to executable" << std::endl;
 
     // create a dockerfile
@@ -50,5 +56,15 @@ image_name_input:
             dockerfile << "WORKDIR " << work_dir << "\n";
     }
     dockerfile.close();
+    
+    std::cout << "build dockerfile? (make sure you are in an empty folder)[y/n]" << std::endl;
+    std::cin >> yesno;
+    switch(yesno) {
+        case 'y':
+            system("sudo docker build .");
+            break;
+        default:
+            break;
+    }
     return 0;
 }
